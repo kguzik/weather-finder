4b0b700145c2f26ad7d215869df40c4df6d4f8b4
kguzik <karolina.guzik08@gmail.com>
2020-09-10 10:04:24 +0200

    Updates

---
+++ precache-manifest.a5815cdf2179cd84de7aa0b067577cf3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b07823b41b2b361d0f8f29ec9a971f3",
+    "url": "/weather-finder/index.html"
+  },
+  {
+    "revision": "fa269fee388189e71d0b",
+    "url": "/weather-finder/static/css/main.a3ca8c21.chunk.css"
+  },
+  {
+    "revision": "fb9b0b56dd861b069c67",
+    "url": "/weather-finder/static/js/2.0f4457f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-finder/static/js/2.0f4457f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa269fee388189e71d0b",
+    "url": "/weather-finder/static/js/main.059bcaf8.chunk.js"
+  },
+  {
+    "revision": "debdbd197d3189e2ce59",
+    "url": "/weather-finder/static/js/runtime-main.a8bbc097.js"
+  },
+  {
+    "revision": "aa04fccfd821fb0dcb49555fe88c939c",
+    "url": "/weather-finder/static/media/background.aa04fccf.jpeg"
+  },
+  {
+    "revision": "994c0b2f9592eed3fb9de9dfc803b692",
+    "url": "/weather-finder/static/media/cloud.994c0b2f.png"
+  }
+]);
